d6f17972aa15b6106cbf4e40ba7155bb4cf42692
Matus Pikuliak <matus.pikuliak@gmail.com>
2020-12-12 10:14:40 +0100

    rotating through complex number exponentiation

--- 12/02.py
+++ 12/02.py
@@ -1,22 +1,15 @@
 dirs = {'E': 1j, 'S': 1, 'W': -1j, 'N': -1}
 rot = {'R': 1, 'L': -1}
 
-pos, wp = 0, -1 + 10j
-
-
-def rot_right(point, num):
-    if num == 0:
-        return point
-    point = point.imag - point.real * 1j
-    return rot_right(point, num - 1)
-
+pos, wp = 0, -1 + 10jd
 
 for line in open('input'):
     c, num = line[0], int(line[1:])
     wp += dirs.get(c, 0) * num
     if c == 'F':
         pos += wp * num
-    wp = rot_right(wp, (rot.get(c, 0) * num // 90) % 4)
+    wp *= 1j ** (-rot.get(c, 0) * num // 90)
+
 
 print(abs(int(pos.imag)) + abs(int(pos.real)))
 
